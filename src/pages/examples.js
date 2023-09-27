d506960114ce988bdbddfc0129df269caf4828ae
Simon Lund <simon97lund@gmail.com>
2023-09-27 16:17:22 +0200

    :sparkles: add mock for ExamplesPage

---
+++ src/pages/examples.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+import Layout from "../components/layout";
+
+// TODO: maybe rename to Showcases
+const ExamplesPage = () => {
+    return (
+        <Layout>
+            <section className={'section'}>
+                <h1>Examples</h1>
+                {/* TODO: add examples/stories/showcases   */}
+            </section>
+        </Layout>
+    )
+}
+
+export default ExamplesPage;
