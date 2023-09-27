0d346f09e9522cc885fc923dc356e224633fc774
Simon Lund <simon97lund@gmail.com>
2023-09-27 16:15:59 +0200

    :clown_face: implement faq with mock text

---
+++ src/pages/faq.js
@@ -0,0 +1,49 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+// TODO: replace with actual data
+const faqs = [
+    {
+        "question": "Lorem ipsum",
+        "answer": "Lorem ipsum"
+    },
+    {
+        "question": "Lorem ipsum",
+        "answer": "Lorem ipsum"
+    },
+    {
+        "question": "Lorem ipsum",
+        "answer": "Lorem ipsum"
+    },
+    {
+        "question": "Lorem ipsum",
+        "answer": "Lorem ipsum"
+    }
+]
+
+const FaqPage = () => {
+    return (
+        <Layout>
+            <section className={'section'}>
+                <h1>F.A.Q.</h1>
+                <div className={'space-y-2'}>
+                    {
+                        faqs.map((faq, index) => (
+                            <div className="collapse collapse-arrow bg-base-200" key={index}>
+                                <input type="radio" name="my-accordion" checked="checked"/>
+                                <div className="collapse-title text-xl font-medium">
+                                    {faq.question}
+                                </div>
+                                <div className="collapse-content">
+                                    <p>{faq.answer}</p>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </section>
+        </Layout>
+    )
+}
+
+export default FaqPage
