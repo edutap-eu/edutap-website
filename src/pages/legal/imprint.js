9c00e5ee9765b2c6e8352ab685513c9548f73ff1
Simon Lund <simon97lund@gmail.com>
2023-09-27 15:38:45 +0200

    :clown_face: add empty pages for privacy policy and imprint

---
+++ src/pages/legal/imprint.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Layout from "../../components/layout";
+
+const ImprintPage = () => (
+    <Layout>
+        <section className="section">
+            <h1 className="text-5xl font-bold">Imprint</h1>
+            <p>
+                {/* TODO */}
+            </p>
+        </section>
+    </Layout>
+);
+
+export default ImprintPage;
---
+++ src/pages/legal/privacy-policy.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Layout from "../../components/layout";
+
+const PrivacyPolicyPage = () => (
+    <Layout>
+        <section className="section">
+            <h1 className="text-5xl font-bold">Privacy Policy</h1>
+            <p>
+                {/* TODO */}
+            </p>
+        </section>
+    </Layout>
+);
+
+export default PrivacyPolicyPage;
