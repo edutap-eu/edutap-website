83c6daa8f5114779efe37b6b63f199af53cada65
Simon Lund <simon97lund@gmail.com>
2023-09-29 01:25:46 +0200

    :recycle: add underconstruction to imprint and privacy policy

--- src/pages/legal/privacy-policy.js
+++ src/pages/legal/privacy-policy.js
@@ -1,13 +1,13 @@
 import * as React from "react";
 import Layout from "../../components/layout";
+import UnderConstruction from "../../components/under-construction";
 
 const PrivacyPolicyPage = () => (
     <Layout>
         <section className="section space-y-4 md:space-y-8 md:text-lg md:mt-12">
             <h1>Privacy Policy</h1>
-            <p>
-                {/* TODO */}
-            </p>
+            <UnderConstruction />
+            {/* TODO */}
         </section>
     </Layout>
 );
