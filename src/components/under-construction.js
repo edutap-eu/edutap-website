bb08b4da2de75c9c0f46aa621f261f605514afe5
Simon Lund <simon97lund@gmail.com>
2023-09-29 01:06:21 +0200

    :art: format

--- src/components/under-construction.js
+++ src/components/under-construction.js
@@ -3,7 +3,7 @@ import {StaticImage} from "gatsby-plugin-image";
 
 const UnderConstruction = () => (
     <div className={'flex flex-col md:flex-row items-center md:items-start space-y-12 space-x-12 px-24 py-16 border-2 border-neutral'}>
-        <StaticImage src={'../images/under-construction.png'} alt={''} class={'max-w-[200px]'}/>
+        <StaticImage src={'../images/under-construction.png'} alt={''} class={'max-w-[200px]'} />
         <div>
             <span className={'text-2xl font-bold'}>Under Construction</span>
             <p>
