039b9098a94c7388a87f9c3221eb5bd975eb366a
Simon Lund <simon97lund@gmail.com>
2023-09-28 23:18:47 +0200

    :lipstick: update footer styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,12 +2,12 @@ import * as React from "react"
 import {Link} from "gatsby";
 
 const Footer = () => (
-    <footer className="flex-none footer footer-center p-4 bg-base-300 text-base-content">
-        <div className={'inline-block space-x-4'}>
+    <footer className="flex-none footer footer-center p-4 pt-8 bg-base-300 text-base-content">
+        <div className={'flex space-x-4'}>
             <Link to={'/legal/privacy-policy'} className={'link link-hover'}>Privacy Policy</Link>
             <Link to={'/legal/imprint'} className={'link link-hover'}>Imprint</Link>
         </div>
-        <aside className={'inline-block md:space-x-12'}>
+        <aside className={'flex flex-col md:flex-row items-center md:space-x-12'}>
                 <span className={'block md:inline'}>
                     © {new Date().getFullYear()}, Built with <a href={'https://www.gatsbyjs.com'}>Gatsby</a>
                 </span>
