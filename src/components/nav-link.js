cfc399a56d12b4b92535d195b9497aa3c453ca9d
Simon Lund <simon97lund@gmail.com>
2023-09-28 13:49:46 +0200

    :fire: remove todo

--- src/components/nav-link.js
+++ src/components/nav-link.js
@@ -3,7 +3,6 @@ import * as React from 'react';
 import {Link} from "gatsby";
 
 const NavLink = ({to, text}) => (
-    // TODO: Rework styling (Checkout designs, improve animation)
     <Link to={to} className={'link link-hover text-lg decoration-4 decoration-primary underline-offset-4'}
           activeClassName={'text-primary'}
     >
