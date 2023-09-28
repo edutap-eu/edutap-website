6e1844c5a02c1af1fa5030dc22a25a6f58eabf4c
Simon Lund <simon97lund@gmail.com>
2023-09-28 12:11:42 +0200

    :iphone: make footer responsive

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,22 +3,18 @@ import {Link} from "gatsby";
 
 const Footer = () => (
     <footer className="flex-none footer footer-center p-4 bg-base-300 text-base-content">
-        <div className={'flex space-x-12'}>
-            <div className={'space-x-4'}>
-                <Link to={'/legal/privacy-policy'} className={'link link-hover'}>Privacy Policy</Link>
-                <Link to={'/legal/imprint'} className={'link link-hover'}>Imprint</Link>
-            </div>
-            <aside className={'space-x-12'}>
-                <span>
+        <div className={'inline-block space-x-4'}>
+            <Link to={'/legal/privacy-policy'} className={'link link-hover'}>Privacy Policy</Link>
+            <Link to={'/legal/imprint'} className={'link link-hover'}>Imprint</Link>
+        </div>
+        <aside className={'inline-block md:space-x-12'}>
+                <span className={'block md:inline'}>
                     © {new Date().getFullYear()}, Built with <a href={'https://www.gatsbyjs.com'}>Gatsby</a>
                 </span>
-                <span>
+            <span className={'block md:inline'}>
                     Illustration(s) from <a className={'link'} href={'https://absurd.design/'}>absurd.design</a>.
                 </span>
-            </aside>
-
-        </div>
-        {/* TODO: Imprint, privacy, contact */}
+        </aside>
     </footer>
 );
 
