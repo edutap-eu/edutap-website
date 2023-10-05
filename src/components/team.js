1c1219943391f4af3a4aed018f9aacf39d56a095
Simon Lund <simon97lund@gmail.com>
2023-10-05 15:27:51 +0200

    :bug: only use one col on small device to prevent breaking layout

--- src/components/team.js
+++ src/components/team.js
@@ -28,7 +28,6 @@ const TeamMember = ({name, org, role, image}) => {
     const info = [org, role].filter(Boolean).join(' • ')
     const img = getImage(image);
 
-    //  
     return (
         <div className={'flex flex-col space-y-2 items-center'}>
             <div className="avatar">
@@ -48,7 +47,7 @@ const Team = () => {
     const data = useStaticQuery(teamQuery);
 
     return (
-        <div className={'grid grid-cols-2 md:grid-cols-3 gap-x-4 lg:gap-x-8 gap-y-4 md:gap-y-8'}>
+        <div className={'grid grid-cols-1 gap-y-8 md:grid-cols-3 gap-x-4 lg:gap-x-8 lg:gap-y-4 md:gap-y-8'}>
             {data.allTeamJson.nodes.map((member, index) => <TeamMember key={index} {...member}/>)}
         </div>
     );
