fc7c7082f276be56ca8f5a90a9f600d971d36526
Simon Lund <simon97lund@gmail.com>
2023-11-09 12:12:42 +0100

    :recycle: rework milestone component
    - update styling
    - add conditional rendering for links (event, presentation & video)

--- src/components/milestone.js
+++ src/components/milestone.js
@@ -2,7 +2,18 @@ import * as React from "react";
 import {VerticalTimelineElement} from "react-vertical-timeline-component";
 
 const Milestone = ({milestone}) => {
-    const {milestone_type=null, date, location= null, title, description, status, event_link=null, presentation_link=null, video_link=null } = milestone;
+    const {
+        milestone_type = null,
+        date,
+        location = null,
+        title,
+        description,
+        status,
+        event_link = null,
+        presentation_link = null,
+        video_link = null
+    } = milestone;
+    console.log(milestone)
     const [color, setColor] = React.useState(status);
 
     // TODO: make colors depend on ui theme
@@ -23,20 +34,41 @@ const Milestone = ({milestone}) => {
     }, [status, title]);
 
     return (
-        <VerticalTimelineElement iconStyle={{background: color}} 
+        <VerticalTimelineElement iconStyle={{background: color}}
                                  date={date}
                                  contentStyle={{border: `2px solid ${color}`}}
                                  contentArrowStyle={{borderRight: `7px solid ${color}`}}
-                                 >
-            {milestone_type !== null && <span>{milestone_type}</span>}
-            <h2>{title}</h2>
-            <p>{description}</p>
-            {event_link !== null && event_link !== ""  && presentation_link !== null && event_link !== "" && video_link !== null && event_link !== "" && <ul class="vertical-list">
-                {event_link !== null && event_link !== ""  && <li><a role="button" class="button" target="_blank" href={event_link} >Event-Website</a></li>}
-                {presentation_link !== null && event_link !== "" && <li><a role="button" class="button" target="_blank" href={presentation_link} >Slides</a></li>}
-                {video_link !== null && event_link !== "" && <li><a role="button" class="button" target="_blank" href={video_link} >YouTube-Video</a></li>}
-            </ul>}
-            <p>{location}</p>
+        >
+            <div>
+                {milestone_type !== null && <span className={'text-base-content/70'}>{milestone_type}</span>}
+                <h2 className={'!mb-4 md:!text-3xl'}>{title}</h2>
+                {description && <p className={'!text-xl !font-light'}>{description}</p>}
+
+                {/*Show if "description is set" AND ("at least one of the links is set" OR "location is set")*/}
+                {description && ([event_link, presentation_link, video_link].some(Boolean) || location) &&
+                    <div className={'border-t mt-4 mb-3'}/>
+                }
+
+                {/*Show if at least one of the links is set*/}
+                {[event_link, presentation_link, video_link].some(Boolean) &&
+                    <div className={'py-3 [&>*]:mb-2 [&>*]:btn [&>*]:btn-sm [&>*]:mr-2'}>
+                        {event_link &&
+                            <a role="button" target="_blank" rel="noreferrer"
+                               href={event_link}>Event-Website</a>
+
+                        }
+                        {presentation_link &&
+                            <a role="button" target="_blank" rel="noreferrer"
+                               href={presentation_link}>Slides</a>
+                        }
+                        {video_link &&
+                            <a role="button" target="_blank" rel="noreferrer"
+                               href={video_link}>YouTube-Video</a>
+                        }
+                    </div>
+                }
+                {location && <span>📍 {location}</span>}
+            </div>
         </VerticalTimelineElement>
     );
 }
